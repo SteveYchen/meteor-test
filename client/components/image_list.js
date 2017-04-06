747a87ee6ad0cb24e45a39bdd08ab64c6f563da5
Steve Chen <Steve Chen>
2017-04-06 05:05:33 -0700

    added image_list component

---
+++ client/components/image_list.js
@@ -0,0 +1,18 @@
+//create our image list component
+
+//import React
+import React from 'react';
+
+//Create our component
+const ImageList = () => {
+	return(
+		<ul>
+			<li>Image Detail</li>
+		</ul>
+	);
+
+};
+
+
+//Export our component
+export default ImageList;
