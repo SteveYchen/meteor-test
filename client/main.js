84628c0c0697d026895c00f22b438de4e9782846
Steve Chen <Steve Chen>
2017-04-07 03:46:17 -0700

    created css, and api call

--- client/main.js
+++ client/main.js
@@ -2,13 +2,17 @@
  
 //import react
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import ImageList from "./components/image_list";
+
+
 
 //create component
 const App = () => {
 	return(
 		<div>
-			React App #2
+			<ImageList /> 
 		</div>
 	);
 }
@@ -16,5 +20,6 @@ const App = () => {
 //render it to screen
 Meteor.startup( () =>{ //wait for dom to finish before loading the reactDOM
 	ReactDOM.render(<App />, document.querySelector('.container'));
-
+	axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+	.then(response => console.log(response));
 });
